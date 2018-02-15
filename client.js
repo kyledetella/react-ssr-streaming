28e99a3d2b12e4081d20ac5e42e080bfb734dd71
Kyle DeTella <kdetella@gmail.com>
2018-02-15 09:00:32 -0600

    Add prettier

--- client.js
+++ client.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const Frontend = (props) => {
+const Frontend = props => {
   return (
     <span>
-    <div>{props.name}</div>
-    <button onClick={() => console.log(`Clicked: ${props.name}`)}>Click Me</button>
+      <div>{props.name}</div>
+      <button onClick={() => console.log(`Clicked: ${props.name}`)}>
+        Click Me
+      </button>
     </span>
-  )
-}
+  );
+};
 
-export default Frontend
+export default Frontend;
